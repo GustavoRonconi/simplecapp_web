06054361570a4dd4d9d5bfaaecb6cc31f3ec0933
GustavoRonconi <gustavoronconi95@gmail.com>
2021-02-05 17:17:54 -0300

    20210205 - Ajuste serializador.

--- app_simpleCapp/serializers/profile_serializer.py
+++ app_simpleCapp/serializers/profile_serializer.py
@@ -1,11 +1,29 @@
 from rest_framework import serializers
 from app_simpleCapp.models.profile_model import ProfileModel
+from django.contrib.auth.models import User
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = (
+            "id",
+            "username",
+            "first_name",
+            "last_name",
+            "email",
+            "password",
+            "last_login",
+            "date_joined",
+        )
 
 
 class ProfileSerializer(serializers.ModelSerializer):
     """
     Serializer for profile user
     """
+
+    user = UserSerializer(default=serializers.CurrentUserDefault())
 
     class Meta:
         model = ProfileModel
--- app_simpleCapp/views/profile_view.py
+++ app_simpleCapp/views/profile_view.py
@@ -5,25 +5,46 @@ from rest_framework import status
 from rest_framework.response import Response
 from app_simpleCapp.models.profile_model import ProfileModel
 from app_simpleCapp.serializers.profile_serializer import ProfileSerializer
+from rest_framework import permissions
+
+
+class IsPostOrIsAuthenticated(permissions.BasePermission):
+    def has_permission(self, request, view):
+        if request.method == "POST":
+            return True
+        return request.user and request.user.is_authenticated
 
 
 class ProfileView(APIView):
-    permission_classes = (IsAuthenticated,)
+    permission_classes = (IsPostOrIsAuthenticated,)
 
     def get(self, request):
         response = {}
 
         profile = ProfileModel.objects.filter(user_id=request.user.id).first()
         if profile is not None:
-            response = {
-                "teste": 1
-            }
+            response = {"teste": 1}
+            return Response(status=status.HTTP_203_NO_CONTENT, data=response)
 
         return Response(status=status.HTTP_204_NO_CONTENT, data=response)
 
     def post(self, request):
-        response = {}
-        serializer = ProfileSerializer(data=request.data)
+        data = request.data
+        serializer = ProfileSerializer(data=data, context={"request": request})
+        if not (request.user.is_authenticated) and "user" not in data.keys():
+            return Response(
+                status=status.HTTP_400_BAD_REQUEST,
+                data={
+                    "errors": "Para criar um perfil sem estar autenticado é necessário informar os dados de usuario."
+                },
+            )
+        else:
+            profile = ProfileModel.objects.filter(user_id=request.user.id).first()
+            if profile is not None:
+                return Response(
+                    status=status.HTTP_400_BAD_REQUEST,
+                    data={"errors": "Já existe um perfil associado a este usuário."},
+                )
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
