3c4a149727ad92562925c7465d070e5c9d57d510
GustavoRonconi <gustavoronconi95@gmail.com>
2021-02-26 13:51:46 -0300

    adicionando postgress

--- api_simpleCapp/settings.py
+++ api_simpleCapp/settings.py
@@ -103,9 +103,21 @@ DATABASES = {
         "USER": "simplecapp",
         "PASSWORD": "simplecappideias",
         "HOST": "localhost",
-        "PORT": os.getenv("POSTGREES_PORT"),
+        "PORT": 5433,
     }
 }
+
+if os.environ.get("GITHUB_WORKFLOW"):
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.postgresql",
+            "NAME": "github_actions",
+            "USER": "postgres",
+            "PASSWORD": "postgres",
+            "HOST": "127.0.0.1",
+            "PORT": "5432",
+        }
+    }
 
 
 # Password validation
