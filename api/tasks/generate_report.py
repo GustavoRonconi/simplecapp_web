e540e1aadd79c8832ca49275871ae079fd683718
GustavoRonconi <gustavoronconi95@gmail.com>
2021-02-28 23:41:57 -0300

    Teste channels com tarefas assincronas

--- api/tasks/generate_report.py
+++ api/tasks/generate_report.py
@@ -1,6 +1,7 @@
 from celery import shared_task
 from time import sleep
 from celery_progress.backend import ProgressRecorder
+from ..utils import send_notifications
 
 
 @shared_task(bind=True)
@@ -9,5 +10,8 @@ def generate_report(self, duration):
     for i in range(5):
         sleep(duration)
         progress_recorder.set_progress(i + 1, 5)
+
+    send_notifications("Não é que esse negócio funciona?", all_user=True)
+
     return "Done"
 
