2c5b1a7ecf3542f56f676eb6f84ef1fc19aea84c
Gustavo A Ronconi <gustavoronconi95@gmail.com>
2021-06-29 18:14:04 -0300

    SP-69 - Criação do modelo financial_operations (sem testes)

---
+++ api/models/financial_operations.py
@@ -0,0 +1,34 @@
+from django.utils.translation import ugettext_lazy as _
+from django.db import models
+
+
+class FinancialOperation(models.Model):
+    class OperationTypes(models.TextChoices):
+        purchase = ("purchase", _("Compra"))
+        sale = ("sale", _("Venda"))
+
+    class OperationClasses(models.TextChoices):
+        day_trade = ("day_trade", _("DayTrade"))
+        normal = ("normal", _("Normal"))
+
+    class TickerTypes(models.TextChoices):
+        stock = ("stock", _("Ações"))
+        fiis = ("fiis", _("Fundos Imobiliários"))
+        bdr = ("bdr", _("BDR"))
+
+    class Meta:
+        db_table = "financial_operation"
+
+    date = models.DateField()
+    operation_type = models.CharField(
+        choices=OperationTypes.choices, max_length=8)
+    operation_class = models.CharField(
+        choices=OperationClasses.choices, max_length=9)
+    ticker = models.CharField(max_length=50)
+    ticker_type = models.CharField(choices=TickerTypes.choices, max_length=5)
+    units = models.DecimalField(max_digits=10, decimal_places=5)
+    unitary_value = models.DecimalField(max_digits=10, decimal_places=5)
+    amount = models.DecimalField(max_digits=10, decimal_places=5)
+    broker = models.ForeignKey(
+        "BrokerModel", null=False, on_delete=models.PROTECT)
+    currency_code = models.CharField(max_length=50)
--- api/models/profile_model.py
+++ api/models/profile_model.py
@@ -6,14 +6,14 @@ class ProfileModel(models.Model):
     class Meta:
         db_table = "profile_user"
 
-    user = models.OneToOneField(User, related_name="profile", on_delete=models.CASCADE)
+    user = models.OneToOneField(
+        User, related_name="profile", on_delete=models.CASCADE)
     gender = models.BooleanField()
-    date_of_birth = models.DateField(null=False)
-    occupation = models.CharField(max_length=50)
-    phone_number = models.CharField(max_length=50, null=False, unique=True)
-    cpf = models.CharField(null=False, max_length=50, unique=True)
-    state = models.ForeignKey("StatesModel", null=False, on_delete=models.PROTECT)
-    cep = models.CharField(null=False, max_length=50)
-    street = models.CharField(max_length=50, null=False)
-    district = models.CharField(max_length=50, null=False)
-
+    date_of_birth = models.DateField()
+    occupation = models.CharField(max_length=50, null=True)
+    phone_number = models.CharField(max_length=50, unique=True)
+    cpf = models.CharField(max_length=50, unique=True)
+    state = models.ForeignKey("StatesModel", on_delete=models.PROTECT)
+    cep = models.CharField(max_length=50)
+    street = models.CharField(max_length=50)
+    district = models.CharField(max_length=50)
